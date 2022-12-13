2c1e7dc58a804b1a26bcf170011b47ea0e93d53a
Guldana <g.abdyjalieva@gmail.com>
2022-12-13 21:48:23 +0300

    Guldana: done layout

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,8 @@
 const hamburger = document.querySelector(".hamburger")
 const listMenu = document.querySelector(".navbar_list")
 
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
   listMenu.classList.toggle("active")
 })
-
-const imageData = [
-  {
-    id:1,
-  }
-]
-
-window.addEventListener("load", () => {
-  ellipseCard(imageData)
-})
-
-
-const symbols = document.querySelector(".navbar_symbols");
-function ellipseCard(base){
-  const template = base.map((image) => {
-    return `
-
-      <img src=${image} />
-    
-    `
-  }).join(" ")
-
-  symbols.innerHTML = template
-}
