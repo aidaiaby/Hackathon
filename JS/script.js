570ae7afc6257353d6c2f37fd81e7bd55f88f230
Guldana <g.abdyjalieva@gmail.com>
2022-12-14 17:48:35 +0300

    Guldana: layout of burger

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,18 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
   listMenu.classList.toggle("active")
 })
+
+
+// Footer //
+
+const footerBanner = document.querySelector(".footer_banner");
+window.addEventListener("load", () => {
+  footerCard()
+})
+function footerCard(base){
+  const footerTemplate = base.map(item => {
+    return `
+    
+    `
+  })
+}
